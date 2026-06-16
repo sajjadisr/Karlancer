3d9d5f62a960dea68540da770f2fa9acf3f4f204
sajjadisr <sajjadi.sr313@gmail.com>
2026-06-16 20:45:20 +0330

    Fixed the config

--- vite.config.js
+++ vite.config.js
@@ -2,34 +2,17 @@ import { defineConfig } from 'vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-<<<<<<< HEAD
-  base: '/Karlancer/',
-  build: {
-=======
   base: './',
   root: '.',
   build: {
     outDir: 'dist',
->>>>>>> 03a838b (Fixed)
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         projects: resolve(__dirname, 'src/pages/projects.html'),
         about: resolve(__dirname, 'src/pages/about.html'),
-        contact: resolve(__dirname, 'src/pages/contact.html'),
-<<<<<<< HEAD
-        projectDetail: resolve(__dirname, 'src/pages/project-detail.html'),
-      },
-    },
-  },
-})
-=======
-        'project-detail': resolve(__dirname, 'src/pages/project-detail.html'),
+        contact: resolve(__dirname, 'src/pages/contact.html')
       }
     }
-  },
-  server: {
-    open: true
   }
 })
->>>>>>> 03a838b (Fixed)
